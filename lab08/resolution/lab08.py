195471d3425f09cd79489be29a8a69e366392588
janozeli <lucasmjanozeli@gmail.com>
2024-05-19 22:27:48 -0300

    src `lab08`

---
+++ lab08/resolution/lab08.py
@@ -0,0 +1,9 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 08 - Calculadora genômica
+# Nome:
+# RA:
+###################################################
+
+
+# ...
